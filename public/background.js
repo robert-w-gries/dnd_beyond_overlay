b29162c423538de7828aefadf25aa59d79b1affb
Rob Gries <robert.w.gries@gmail.com>
2020-02-28 15:34:03 -0500

    Inject sidebar only for Roll20 pages

--- public/background.js
+++ public/background.js
@@ -1,7 +1,5 @@
-chrome.storage.local.set({ lifeCycle: 'start' });
-
 chrome.browserAction.onClicked.addListener(function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.executeScript(tabs[0].id, {file: 'inject.js'});
+        chrome.tabs.sendMessage(tabs[0].id,"toggle");
     })
 });
