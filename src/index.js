47bf33b3002aa804f656ae1759cc4b57ac97ec06
Jake Chitel <jchitel@gmail.com>
2020-03-22 14:28:39 -0500

    TEMP: removing all files for review purposes

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Overlay from './ui/Overlay';
-
-ReactDOM.render(<Overlay />, document.getElementById('root'));
